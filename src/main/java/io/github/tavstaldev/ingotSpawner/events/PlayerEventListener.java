8e355d5ef225667d1123c83e28a66cbc7632acbe
tavstal <tavstal@gmail.com>
2025-10-17 15:01:48 +0200

    add player event listener for item pickup to reward players with in-game currency

--- src/main/java/io/github/tavstaldev/ingotSpawner/events/PlayerEventListener.java
+++ src/main/java/io/github/tavstaldev/ingotSpawner/events/PlayerEventListener.java
@@ -1,14 +1,27 @@
 package io.github.tavstaldev.ingotSpawner.events;
 
+import io.github.tavstaldev.ingotSpawner.IngotSpawner;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Item;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+
 public class PlayerEventListener implements Listener {
+
+    public void register() {
+        Bukkit.getPluginManager().registerEvents(this, IngotSpawner.Instance);
+    }
+
     @EventHandler
     public void onPlayerPickupItem(PlayerPickupItemEvent event) {
         Item item = event.getItem();
-        if (item.getItemStack().getType() == Material.GOLD_INGOT) {
-            event.setCancelled(true);
-            item.remove();
-            String command = this.configManager.getRewardCommand().replace("{player}", event.getPlayer().getName());
-            Bukkit.dispatchCommand((CommandSender)Bukkit.getConsoleSender(), (String)command);
-        }
+        var ingot = IngotSpawner.config().getIngotByMaterial(item.getItemStack().getType());
+        if (ingot == null)
+            return;
+
+        event.setCancelled(true);
+        item.remove();
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), String.format("eco give %s %.2f", event.getPlayer().getName(), ingot.getMoney()));
     }
 }
