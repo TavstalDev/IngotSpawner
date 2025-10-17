3b0b16b2bb131fb67b5bedf0963e1a6006348e1a
tavstal <tavstal@gmail.com>
2025-10-17 15:02:09 +0200

    add IngotSpawner class enhancements for task scheduling and event handling

--- src/main/java/io/github/tavstaldev/ingotSpawner/IngotSpawner.java
+++ src/main/java/io/github/tavstaldev/ingotSpawner/IngotSpawner.java
@@ -1,23 +1,32 @@
 package io.github.tavstaldev.ingotSpawner;
 
+import io.github.tavstaldev.ingotSpawner.commands.CommandIngot;
+import io.github.tavstaldev.ingotSpawner.events.PlayerEventListener;
+import io.github.tavstaldev.ingotSpawner.task.SpawnTask;
 import io.github.tavstaldev.minecorelib.PluginBase;
 import io.github.tavstaldev.minecorelib.core.PluginLogger;
 import io.github.tavstaldev.minecorelib.core.PluginTranslator;
+import org.bukkit.Bukkit;
 
 public final class IngotSpawner extends PluginBase {
-
     public static IngotSpawner Instance;
+    private IngotLocations locations;
+    private SpawnTask spawnTask;
 
-    public static PluginLogger Logger() {
+    public static PluginLogger logger() {
         return Instance.getCustomLogger();
     }
 
-    public static PluginTranslator Translator() {
+    public static PluginTranslator translator() {
         return Instance.getTranslator();
     }
 
-    public static IngotSpawnerConfig Config() {
+    public static IngotSpawnerConfig config() {
         return (IngotSpawnerConfig) Instance._config;
+    }
+
+    public static IngotLocations getLocations() {
+        return Instance.locations;
     }
 
     public IngotSpawner() {
@@ -30,34 +39,43 @@ public final class IngotSpawner extends PluginBase {
         _config = new IngotSpawnerConfig();
         _config.load();
         _translator = new PluginTranslator(this, new String[]{"eng", "hun"});
-        _logger.Info(String.format("Loading %s...", getProjectName()));
+        locations = new IngotLocations();
+        locations.load();
+        _logger.info(String.format("Loading %s...", getProjectName()));
 
         // Load localizations.
-        if (!_translator.Load()) {
-            _logger.Error("Failed to load localizations... Unloading...");
+        if (!_translator.load()) {
+            _logger.error("Failed to load localizations... Unloading...");
             Bukkit.getPluginManager().disablePlugin(this);
             return;
         }
 
         // Register commands.
-        _logger.Debug("Registering commands...");
-        var command = getCommand("openchat");
-        if (command != null) {
-            command.setExecutor(new CommandChat());
-        }
+        _logger.debug("Registering commands...");
+        new CommandIngot();
 
-        _logger.Ok(String.format("%s has been successfully loaded.", getProjectName()));
+        // Register event listeners.
+        new PlayerEventListener().register();
+
+        // Register tasks
+        if (spawnTask != null && !spawnTask.isCancelled()) {
+            spawnTask.cancel();
+        }
+        spawnTask = new SpawnTask();
+        spawnTask.runTaskTimer(this, 0L, 20 * 60 * 5);
+
+        _logger.ok(String.format("%s has been successfully loaded.", getProjectName()));
 
         // Check for plugin updates if enabled in the configuration.
         if (getConfig().getBoolean("checkForUpdates", true)) {
             isUpToDate().thenAccept(upToDate -> {
                 if (upToDate) {
-                    _logger.Ok("Plugin is up to date!");
+                    _logger.ok("Plugin is up to date!");
                 } else {
-                    _logger.Warn("A new version of the plugin is available: " + getDownloadUrl());
+                    _logger.warn("A new version of the plugin is available: " + getDownloadUrl());
                 }
             }).exceptionally(e -> {
-                _logger.Error("Failed to determine update status: " + e.getMessage());
+                _logger.error("Failed to determine update status: " + e.getMessage());
                 return null;
             });
         }
@@ -65,18 +83,24 @@ public final class IngotSpawner extends PluginBase {
 
     @Override
     public void onDisable() {
-        _logger.Info(String.format("%s has been successfully unloaded.", getProjectName()));
+        _logger.info(String.format("%s has been successfully unloaded.", getProjectName()));
     }
 
     public void reload() {
-        _logger.Info(String.format("Reloading %s...", getProjectName()));
-        _logger.Debug("Reloading localizations...");
-        _translator.Load();
-        _logger.Debug("Localizations reloaded.");
-        _logger.Debug("Reloading configuration...");
+        _logger.info(String.format("Reloading %s...", getProjectName()));
+        _logger.debug("Reloading localizations...");
+        _translator.load();
+        _logger.debug("Localizations reloaded.");
+        _logger.debug("Reloading configuration...");
         this._config.load();
-        _logger.Debug("Configuration reloaded.");
+        _logger.debug("Configuration reloaded.");
 
-        _logger.Ok(String.format("%s has been successfully reloaded.", getProjectName()));
+        if (spawnTask != null && !spawnTask.isCancelled()) {
+            spawnTask.cancel();
+        }
+        spawnTask = new SpawnTask();
+        spawnTask.runTaskTimer(this, 0L, 6000L);
+
+        _logger.ok(String.format("%s has been successfully reloaded.", getProjectName()));
     }
 }
